699c4cd58585b72c95f2d5ab53c8751b4d9767fd
Rodrigo <tu-email@example.com>
2025-06-21 22:48:18 -0400

    act

---
+++ perfulandia/sucursales/src/main/java/com/perfulandia/sucursales/PerfulandiaApplicationTests.java
@@ -0,0 +1,15 @@
+/* package com.perfulandia.perfulandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+/* */
